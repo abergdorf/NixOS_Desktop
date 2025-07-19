405fb1953d559444ecf0c341b960803fc8901c76
andrew <abergy@gmail.com>
2025-07-19 12:55:50 -0500

    add filesystem.nix with UUID

---
+++ filesystem.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+
+#uuid of 8tb 940f4332-3aaf-4e83-a244-5d0e3f788569
+  fileSystems."/media" = { # Choose your desired mount point
+    device = "/dev/disk/by-uuid/940f4332-3aaf-4e83-a244-5d0e3f788569"; # Replace with your actual UUID
+    fsType = "ext4"; # Replace with your filesystem type (e.g., "btrfs", "xfs")
+    options = [ "defaults" "nofail" ]; # Common options, "nofail" is useful for HDDs
+  };
+
+};
