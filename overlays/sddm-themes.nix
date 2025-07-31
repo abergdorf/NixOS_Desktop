853a57db85c9fe0215951a5e5747f3e793405ef5
andrew <abergy@gmail.com>
2025-07-31 08:22:06 -0500

    fake sha again

--- overlays/sddm-themes.nix
+++ overlays/sddm-themes.nix
@@ -9,7 +9,7 @@ self: super: {
       rev = "v${version}";
       # The sha256 you provided is likely outdated.
       # Replace the hash below with the one Nix gives you after a failed build.
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
 
     dontBuild = true;
