002f3fed3dd3c69f314d68c4954205a1b629ef69
andrew <abergy@gmail.com>
2026-03-14 09:23:15 -0500

    update syntax, update mount point

--- filesystem.nix
+++ filesystem.nix
@@ -12,30 +12,33 @@
 #service needed to get network drive
 services.samba = {
   enable = true;
-  securityType = "user";
-  extraConfig = ''
-    workgroup = WORKGROUP
-    server string = NixOS-NAS
-    netbios name = NixOS-NAS
-    security = user
-    # This allows Windows to discover the share more easily
-    hosts allow = 192.168.1. 127.0.0.1 localhost
-    hosts deny = 0.0.0.0/0
-  '';
-  shares = {
+  # nmbd is often needed for NetBIOS name resolution (NixOS-NAS)
+  nmbd.enable = true;
+
+  settings = {
+    global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "NixOS-NAS";
+      "netbios name" = "NixOS-NAS";
+      "security" = "user";
+      "hosts allow" = "192.168.1. 127.0.0.1 localhost";
+      "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+    };
     storage = {
-      path = "/mnt/storage";
-      browseable = "yes";
+      "path" = "/media";
+      "browseable" = "yes";
       "read only" = "no";
       "guest ok" = "no";
       "create mask" = "0644";
       "directory mask" = "0755";
-      "force user" = "andrew"; # Use your NixOS username
+      "force user" = "andrew";
     };
   };
 };
 
-# Enable WSDD to make the drive show up in Windows "Network" explorer
+# Keep this for Windows discovery
 services.samba-wsdd.enable = true;
 networking.firewall.allowPing = true;
 networking.firewall.allowedTCPPorts = [ 445 139 ];
