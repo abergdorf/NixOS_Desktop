22e866bac6353d7e73030a88162abf34122df448
andrew <abergy@gmail.com>
2026-07-04 14:55:51 -0500

    add appimage support

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,10 @@ systemd = {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  #appimage setup
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+  
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
