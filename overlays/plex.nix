8bff060bf0adbeeccd16518b5ab7f156fefc9d1c
andrew <abergy@gmail.com>
2026-08-11 08:04:58 -0500

    add noctalia, update plex

--- overlays/plex.nix
+++ overlays/plex.nix
@@ -2,13 +2,13 @@ self: super:
 
 {
  plexRaw = super.plexRaw.overrideAttrs (oldAttrs: rec {
-    version = "1.42.1.10060-4e8b05daf";
+    version = "1.43.2.10687-563d026ea";
 
     # The new source URL
     src = super.fetchurl {
       url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
       # The hash you generated with nix-prefetch-url
-      sha256 = "1x4ph6m519y0xj2x153b4svqqsnrvhq9n2cxjl50b9h8dny2v0is";
+      sha256 = "13mfmlwvpimyrm3dkdlsr0b9qpbyy5l62q2ckh1xvzgj978j62bn";
     };
 
     # Update other attributes if necessary (e.g., source hash)
@@ -17,3 +17,5 @@ self: super:
     pname = "plexmediaserver";
   });
 }
+  #do the following command in terminal to get the SHA. Replace the version with the one you want to update to
+  #nix-prefetch-url https://downloads.plex.tv/plex-media-server-new/1.43.2.10687-563d026ea/debian/plexmediaserver_1.43.2.10687-563d026ea_amd64.deb
